3e6215d060dc76af4c8c2a81930e2d562a0b839e
Dana Yatsuta <erissyatsuta@gmail.com>
2021-02-19 23:31:18 +0300

    Fixed Flight migration

--- db/migrate/20210219202316_create_flights.rb
+++ db/migrate/20210219202316_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
-      t.references :origin_airport, null: false, foreign_key: true
-      t.references :destination_airport, null: false, foreign_key: true
+      t.references :origin_airport, null: false
+      t.references :destination_airport, null: false
       t.date :start
       t.integer :duration
 
