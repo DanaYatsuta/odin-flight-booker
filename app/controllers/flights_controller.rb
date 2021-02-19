295682d1071fbcb737204907b21df3fc8e58df51
Dana Yatsuta <erissyatsuta@gmail.com>
2021-02-20 02:08:04 +0300

    Retain values in search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,8 @@
 class FlightsController < ApplicationController
   def index
     if params.has_key?(:query)
+      @query = params[:query]
+
       @flights = Flight.where(
         origin_airport_id: params[:query][:origin_airport_id],
         destination_airport_id: params[:query][:destination_airport_id],
