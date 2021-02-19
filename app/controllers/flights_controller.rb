82f1d91393bbf4ab38a85c54b8ef5151a4ce3e61
Dana Yatsuta <erissyatsuta@gmail.com>
2021-02-20 01:47:23 +0300

    Implemented flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
-    @airport_codes = Airport.pluck(:id, :code).to_h
+    if params.has_key?(:query)
+      @flights = Flight.where(
+        origin_airport_id: params[:query][:origin_airport_id],
+        destination_airport_id: params[:query][:destination_airport_id],
+        start: params[:query][:start]
+      )
+    else
+      @flights = Flight.all
+    end
+
+    @airport_options = Airport.pluck(:id, :code)
+    @airport_codes = @airport_options.to_h
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :origin_airport, class_name: 'Airport'
   belongs_to :destination_airport, class_name: 'Airport'
+
+  def start_date_formatted
+    start.strftime('%d.%m.%Y')
+  end
 end
