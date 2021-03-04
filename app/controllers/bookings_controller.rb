ba816b556b36a70009bac11c2f495c377988de7b
Dana Yatsuta <erissyatsuta@gmail.com>
2021-03-04 20:53:22 +0300

    Set up "new" action and view for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+
+    flight_to_book_id = params[:flight_to_book_id].to_i
+    passenger_count = params[:passenger_count].to_i
+    passenger_count.times { @booking.passengers.build }
+
+    @flight_to_book = Flight.find(flight_to_book_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'flights#index'
 
   resources :flights, only: [:index]
-  resources :bookings, only: [:new]
+  resources :bookings, only: [:new, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
