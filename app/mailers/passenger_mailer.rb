ccbdec05f05e32d936ba93c21b01d202e98290a4
Dana Yatsuta <danayatsuta@gmail.com>
2021-03-24 10:12:02 +0300

    Fixed passenger mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,7 +1,8 @@
 class PassengerMailer < ApplicationMailer
-default from: 'notifications@example.com'
+  default from: 'notifications@example.com'
 
-def thank_you_email
-  @user = params[:user]
-  mail(to: @user.email, subject: 'Thank you for booking a flight!')
+  def thank_you_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Thank you for booking a flight!')
+  end
 end
