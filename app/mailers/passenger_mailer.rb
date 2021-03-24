edc393a94a14da02a482d493be581d73ebd50019
Dana Yatsuta <danayatsuta@gmail.com>
2021-03-24 09:34:57 +0300

    Defined "thank you email" action for mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+default from: 'notifications@example.com'
+
+def thank_you_email
+  @users = params[:users]
+  @users.each do |user|
+    mail(to: user.email, subject: 'Thank you for booking a flight!')
+  end
 end
